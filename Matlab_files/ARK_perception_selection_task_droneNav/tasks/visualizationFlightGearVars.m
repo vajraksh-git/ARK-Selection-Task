1b5ddbc33446f56fe4dc4a2f46e66502ce365ea9
vajraksh-git <pymailbrocode@gmai.com>
2026-03-09 21:40:25 +0530

    added matlab_files

---
+++ Matlab_files/ARK_perception_selection_task_droneNav/tasks/visualizationFlightGearVars.m
@@ -0,0 +1,13 @@
+%% Visualization Block Variables
+% Generate Run Script
+
+% Copyright 2013-2024 The MathWorks, Inc.
+
+Visualization.FlightGear.generateRunScript.initialAltitude = '7224';
+Visualization.FlightGear.generateRunScript.initialHeading = '113';
+Visualization.FlightGear.generateRunScript.offsetDistance = '4.72';
+Visualization.FlightGear.generateRunScript.offsetAzimuth = '0';
+Visualization.FlightGear.generateRunScript.destinationPort = '5502';
+
+% Preconfigured 6DOF
+Visualization.FlightGear.SixDOF.destinationPort = '5502';
